e4127c1f4f3fb9afd6c58b943a83402af9586802
DanielVllmzr <126840617+DanielVllmzr@users.noreply.github.com>
2025-09-02 17:00:10 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-const ScanCheck = dynamic(() => import('@/components/ScanCheck'), { ssr: false });
+const ScanCheck = dynamic(() => import('../components/ScanCheck'), { ssr: false });
 
 export default function Page() {
   return <ScanCheck />;
