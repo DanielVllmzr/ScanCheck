cd70c476cc1d4940295406f838e28437d3deef4d
DanielVllmzr <126840617+DanielVllmzr@users.noreply.github.com>
2025-09-02 16:34:41 -0400

    Delete page.tsx

--- page.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const ScanCheck = dynamic(() => import('@/components/ScanCheck'), { ssr: false });
-
-export default function Page() {
-  return <ScanCheck />;
-}
